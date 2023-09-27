a3735c4aa493a60b47843faca04fb44d176ec075
user_name <email_id>
2023-09-28 01:02:31 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-function addingEventListener() {
-}
+   // index.js
+function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    // Get the input element by its ID
+    const input = document.getElementById('input');
+  
+    // Add a click event listener to the input element
+    input.addEventListener('click', clickAlert);
+  
+    // Get the button element by its ID
+    const button = document.getElementById('button');
+  
+    // Add a click event listener to the button element using an anonymous function
+    button.addEventListener('click', function() {
+      alert('Button was clicked!');
+    });
+  });
+  
